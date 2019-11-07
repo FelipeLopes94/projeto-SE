7daa0e905d117daec936fb5740b797e563ff78c6
Felipe Lopes <felipee.c.lopess@gmail.com>
2019-11-07 12:23:22 -0300

    Ajustes rotas

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@ import React from 'react';
 import './App.css';
 import Cards from './components/Card'
 import Nav from './components/Navbar'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Nav></Nav>
-      <h1 className="titulo">Lugares</h1>
-      <p className="subtitulo">X Lugares cadastrados</p>
-      <div class="col-lg-6">
-        <div className="cross"><a>⊕</a> </div>
-        <Cards></Cards>
-        <div className="cross"><a>⊕</a></div>
-        <Cards></Cards>
-        <div className="cross"><a>⊕</a></div>
-        <Cards></Cards>
-        <div className="cross"><a>⊕</a></div>
-        <Cards></Cards>
-        <div className="cross"><a>⊕</a></div>
-        <Cards></Cards>
-      </div>
+    <Router>
+      <div className="App">
 
-    </div>
+        <Nav></Nav>
+        <h1 className="titulo">Lugares</h1>
+        <p className="subtitulo">X Lugares cadastrados</p>
+
+        <div class="col-lg-6">
+          <div className="cross">⊕ </div>
+          <Cards></Cards>
+          <div className="cross">⊕</div>
+          <Cards></Cards>
+          <div className="cross">⊕</div>
+          <Cards></Cards>
+          <div className="cross">⊕</div>
+          <Cards></Cards>
+          <div className="cross">⊕</div>
+          <Cards></Cards>
+        </div>
+
+      </div>
+    </Router>
+
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
 
+import { Link } from 'react-router-dom';
+
+
+const links = [
+    { route: "/", label: "Home" },
+    { route: "/informacoes", label: "Informacoes" },
+    { route: "/cadastro", label: "Cadastro" },
+];
 class Cards extends React.Component {
+
+    renderLink = () => {
+        return links.map(link =>
+            <Link key={link.route} className="nav-link" to={link.route}>
+                {link.label}
+            </Link>
+        )
+    }
     state = {}
     render() {
         return (
             <Card className="bar">
-                <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
-                    <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
-                     </Card.Text>
-                </Card.Body>
+                <ul>
+                    <Card.Body>
+                        <Card.Title>Special title treatment</Card.Title>
+                        <Card.Text>
+                            {this.renderLink()}
+                        </Card.Text>
+                    </Card.Body>
+                </ul>
+
             </Card>
 
         );
